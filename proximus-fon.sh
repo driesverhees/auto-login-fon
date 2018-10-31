501832d7ff37652b45f895bc9c6dcf0069311be0
dries <github@dverhees.be>
2018-11-01 00:11:16 +0100

    Adapted for new proximus fon portal

--- proximus-fon.sh
+++ proximus-fon.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
-
-wget -qO- --output-document=blank_page.html --no-check-certificate --save-cookies=cookie --keep-session-cookies http://www.google.com/blank.html
-
+# Username and password of the FON account (url encoded!)
+FON_USERNAME='xxxxxxxx'
+FON_PASSWORD='xxxxxxxx'
+wget -qO- --output-document=blank_page.html http://www.google.com/blank.html
 if (cat blank_page.html | grep -q -i "proximus"); then
 	POST_ACTION=$(cat blank_page.html | grep -i 'action="https://proximus' | cut -d \" -f 4)
 	if [ -n "$POST_ACTION" ]; then
-		wget -qO- --output-document=return.html --no-check-certificate --load-cookies=cookie --keep-session-cookies --post-data="UserName=YOUR_FON_ACCOUNT&Password=YOUR_FON_PASSWORD&rememberMe=true&_rememberMe=on&login=Login" $POST_ACTION
+		# Replace encoded &amp; in the URL by &
+		CLEANED_POST_ACTION=$(echo $POST_ACTION | sed 's/\b\&amp;\b/\&/g')
+		# Send POST request		
+		wget -qO- --output-document=return.html --post-data="UserName=$FON_USERNAME&Password=$FON_PASSWORD&chooseUser=passuser&rememberMe=true&_rememberMe=on&login=Login&language=en_GB" "$CLEANED_POST_ACTION"
 		if (cat return.html | grep -q -i "success"); then
-			logger "FON-network: Login successfull"
+			echo "FON-network: Login successfull"
 		else
-			logger "FON-network: Login failed"
+			echo "FON-network: Login failed"
 		fi
 		rm return.html
 	else
-		logger "FON-network: Unable to get post url"
+		echo "FON-network: Unable to get post url"
 
 	fi
 fi
-rm cookie blank_page.html
+rm blank_page.html
