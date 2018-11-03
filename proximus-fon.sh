ff6d032ee552a1f8cffeb5012037421c6da75a3e
dries <github@dverhees.be>
2018-11-03 16:49:12 +0100

    No check of certificates

--- proximus-fon.sh
+++ proximus-fon.sh
@@ -2,14 +2,14 @@
 # Username and password of the FON account (url encoded!)
 FON_USERNAME='xxxxxxxx'
 FON_PASSWORD='xxxxxxxx'
-wget -qO- --output-document=blank_page.html http://www.google.com/blank.html
+wget -qO- --no-check-certificate --output-document=blank_page.html http://www.google.com/blank.html
 if (cat blank_page.html | grep -q -i "proximus"); then
 	POST_ACTION=$(cat blank_page.html | grep -i 'action="https://proximus' | cut -d \" -f 4)
 	if [ -n "$POST_ACTION" ]; then
 		# Replace encoded &amp; in the URL by &
 		CLEANED_POST_ACTION=$(echo $POST_ACTION | sed 's/\b\&amp;\b/\&/g')
 		# Send POST request		
-		wget -qO- --output-document=return.html --post-data="UserName=$FON_USERNAME&Password=$FON_PASSWORD&chooseUser=passuser&rememberMe=true&_rememberMe=on&login=Login&language=en_GB" "$CLEANED_POST_ACTION"
+		wget -qO- --no-check-certificate --output-document=return.html --post-data="UserName=$FON_USERNAME&Password=$FON_PASSWORD&chooseUser=passuser&rememberMe=true&_rememberMe=on&login=Login&language=en_GB" "$CLEANED_POST_ACTION"
 		if (cat return.html | grep -q -i "success"); then
 			logger "FON-network: Login successfull"
 		else
